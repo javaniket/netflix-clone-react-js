d35aeb12b4c380e8166336b9349221558e134505
javaniket <124253293+javaniket@users.noreply.github.com>
2023-03-25 11:57:27 +0530

    all files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
-import Home from './Home';
-import NetFlixShow from './NetFlixShow';
+import NetFlixShow from './pages/NetFlixShow';
+import Home from "./pages/Home";
+import Footer from './components/Footer/Footer';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      
+      <ScrollToTop />
       <Routes>
       <Route path="/" element={ <Home/> } />
       <Route path="/netflix-show" element={ <NetFlixShow/> } />
     </Routes>
+    <Footer />
     </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Header from './components/Header/Header';
-import TabComponent from "./components/Tabs";
+import React from 'react';
+import Header from '../components/Header/Header';
+import TabComponent from "../components/Tabs";
 
 const Home = () => {
   return (
